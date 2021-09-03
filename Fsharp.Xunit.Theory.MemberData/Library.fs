b6d21c30941a8c804e613e7ae1fe389afefe69bb
Tomasz Sztokinier <tomasz@todoit.pl>
2021-09-03 12:57:24 +0200

    code showing the problem

---
+++ Fsharp.Xunit.Theory.MemberData/Library.fs
@@ -0,0 +1,13 @@
+﻿module Fsharp.Xunit.Theory.MemberData
+open Xunit
+
+let someVals = ["abba"]
+
+type Fixture() =
+    [<Fact>]
+    member _.AssertIsOnlyAbba() = Assert.Equal<string>(["abba"], someVals)
+
+// uncomment the code below - tests should start working
+
+//[<EntryPoint>]
+//let main (args:string[]) = 0
